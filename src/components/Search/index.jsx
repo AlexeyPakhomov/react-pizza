43599eeee51b9d1604e509eb17314b0f18dd196e
Alexey Pakhomov <pakhomovs.mail@yandex.ru>
2024-09-10 15:56:21 +0300

    add focus searchInput after clear searchInput

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,11 +1,17 @@
 import style from './Search.module.scss';
 import searchImg from '../../assets/img/search_icon.svg';
 import removeIcon from '../../assets/img/remove_icon.svg';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { searchContext } from '../../App';
 
 const Search = () => {
+  const searchRef = useRef();
   const { searchValue, setSearchValue } = useContext(searchContext);
+
+  function handleClearInput() {
+    setSearchValue('');
+    searchRef.current.focus();
+  }
 
   return (
     <div className={style.searchBlock}>
@@ -15,13 +21,14 @@ const Search = () => {
         alt="search"
       />
       <input
+        ref={searchRef}
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
         className={style.input}
         placeholder="Введите для поиска..."
       />
       <img
-        onClick={() => setSearchValue('')}
+        onClick={() => handleClearInput()}
         className={style.searchBlock__iconRemove}
         src={removeIcon}
         alt="remove"
